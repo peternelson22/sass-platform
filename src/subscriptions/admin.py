918e2fac0b2f1a33bb6534deb9ce12b2889221c4
Nelson <peternelson063@gmail.com>
2024-09-08 11:23:40 +0100

    Added subscription flow with stripe

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Subscription, UserSubscription
+from .models import Subscription, UserSubscription, SubscriptionPrice
 
-admin.site.register(Subscription)
+class SubscriptionPrice(admin.StackedInline):
+    model = SubscriptionPrice
+    readonly_fields = ['stripe_id']
+    can_delete = False
+    extra = 0
+class SubscriptionAdmin(admin.ModelAdmin):
+    inlines = [SubscriptionPrice]
+    readonly_fields = ['stripe_id']
+    list_display = ['name', 'active']
+    
+    
+admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(UserSubscription)
 
 # Register your models here.
