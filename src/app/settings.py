da3411e0767579d15a3a551e3d5a8b22ead8959f
Nelson <peternelson063@gmail.com>
2024-09-11 10:23:39 +0100

    Modified Django test with environment variables

--- src/app/settings.py
+++ src/app/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [ config('ALLOWED_HOSTS') ]
+ALLOWED_HOSTS = [ '*' ]
  
 if DEBUG:
     ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
