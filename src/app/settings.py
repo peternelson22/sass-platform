b9803e79ce2740e995232d6a20b450da885d36a1
Nelson <peternelson063@gmail.com>
2024-09-12 19:52:53 +0100

    Added dashboard page

--- src/app/settings.py
+++ src/app/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     
     # my apps
     'landing',
+    'dashboard',
     'command',
     'visits',
     'profiles',
--- src/landing/views.py
+++ src/landing/views.py
@@ -2,7 +2,11 @@ import helpers.numbers
 from django.shortcuts import render
 from visits.models import PageVisit
 
+from dashboard.views import dashboard_view
+
 def landing_page_view(request):
+    if request.user.is_authenticated:
+        return dashboard_view(request)
     qs = PageVisit.objects.all()
     page_views_formatted = helpers.numbers.shorten_number(qs.count() * 100_000)
     social_page_views_formatted = helpers.numbers.shorten_number(qs.count() * 40_000)
